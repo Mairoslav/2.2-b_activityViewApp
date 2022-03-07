0d38256194be208d688aea8bfa11679dbe9bfeee
Miroslav Mikovec <miroslav.mikovec@gmail.com>
2022-03-07 21:29:47 +0100

    Initial Commit

---
+++ 2.2 b_activityViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.2 b_activityViewApp
+//
+//  Created by mairo on 07/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
