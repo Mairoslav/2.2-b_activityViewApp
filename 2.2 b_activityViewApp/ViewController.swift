ac740e0274ece67d7838914c5e88f5383ccfb31f
Miroslav Mikovec <miroslav.mikovec@gmail.com>
2022-03-07 23:28:54 +0100

    version I.

--- 2.2 b_activityViewApp/ViewController.swift
+++ 2.2 b_activityViewApp/ViewController.swift
@@ -14,6 +14,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func experiment() {
+        let image = UIImage()
+        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        present(controller, animated: true, completion: nil) // self.present works, can skip
+    }
 }
+
+/*
+ In AppDelegate.swift file folowing error occurs when I run this code for iPad instead of iPhone:
+ 
+ Thread 1: "UIPopoverPresentationController (<UIPopoverPresentationController: 0x123007290>) should have a non-nil sourceView or barButtonItem set before the presentation occurs."
+ */
+
+/*
+ when using:
+ (sender: Any)
+ 
+ Error:
+ Thread 1: "-[__2_b_activityViewApp.ViewController experiment]: unrecognized selector sent to instance 0x1598059c0"
+ 
+ when using:
+ (sender: UIButton)
+ 
+ Error:
+ Thread 1: "-[__2_b_activityViewApp.ViewController experiment]: unrecognized selector sent to instance 0x153808360"
+ */
 
